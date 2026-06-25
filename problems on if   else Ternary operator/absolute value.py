4c4ff2873af82242cba348fe76bce4dffe87f116
Chakinala Aman <amanchakinala37@gmail.com>
2026-06-25 13:17:36 +0530

    Delete problems on if   else Ternary operator directory

--- problems on if   else Ternary operator/absolute value.py
+++
@@ -1,7 +0,0 @@
-#program to find the absolute value of a number using the if..else operator.
-#Ex-10       absolute value of a number is always positive or zero
-a=float(input("Enter a value:"))
-
-res= a*-1 if a<=0  else a
-
-print("Absolute value of {} is {}".format(a,res))
--- problems on if   else Ternary operator/pass or fail.py
+++
@@ -1,8 +0,0 @@
-# a program to determine whether a student has passed or failed based on marks using the if..else operator
-#Ex-7
-a=int(input("Enter U R Marks Number:"))
-
-res= "Student is passed" if a>=35 else "Student is failed"
-
-print("Entered Marks are {} That is {}".format(a,res))
-
