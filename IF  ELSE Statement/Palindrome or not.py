06be12c54e94b5222b6d5f051de53093f9fab620
Chakinala Aman <amanchakinala37@gmail.com>
2026-06-29 20:41:37 +0530

    Delete IF  ELSE Statement directory

--- IF  ELSE Statement/Palindrome or not.py
+++
@@ -1,8 +0,0 @@
-#Program to implement whether given input is palindrome or not palindrome
-#Ex-1
-value=input("Enter any value:")
-if(value==value[::-1]):
-    print("\t{} is a palindrome".format(value))
-else:
-    print("\t{} is not a palindrome".format(value))
-print("Execution of Program has been completed!!!!!!!")
